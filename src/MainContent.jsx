1e6256092e78887b0c843887ca965179d7510fd3
CRIMSON <olueatoworosanwo@gmail.com>
2022-09-26 16:51:25 +0100

    minor refactoring

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,87 +1,78 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/header";
 import Homepage from "./components/Homepage";
 import RenderDie from "./components/renderDie";
 import RollBtn from "./components/RollBtn";
 
-function MainContent() {
-  const RandomNumber = () => {
-    const newArr = [];
-    for (let i = 1; i < 11; i++) {
-      let num = Math.floor(Math.random() * 6) + 1;
-      let object = {
-        value: num,
-        isHeld: false,
-        id: i,
-      };
-      newArr.push(object);
-    }
-console.log('new game')
-    return newArr;
-  };
-
-  const [Value, setValue] = useState(RandomNumber());
-
-  const regenerateNewArray = () => {
-    setValue((value) => {
-      return value.map((e, index) =>
-        e.isHeld
-          ? e
-          : (() => {
-              let num = Math.floor(Math.random() * 6) + 1;
-              let object = {
-                value: num,
-                isHeld: false,
-                id: index * 10,
-              };
-              return object;
-            })()
-      );
-    });
-  };
-
-  const toggleIsHeld = (id) => {
-    setValue((prevState) => {
-      return prevState.map((e) => {
-        return e.id === id ? { ...e, isHeld: !e.isHeld } : e;
-      });
-    });
-  };
-
-  const [tenzies, setTenzies] = useState(false);
-
-  console.log(Value);
-
-  useEffect(() => {
-    console.log("dice state changed");
-    const allHeld = Value.every((e) => e.isHeld === true);
-    const firstNumber = Value[0].value;
-    const sameNumber = Value.every((e) => e.value === firstNumber);
-
-    if (allHeld&&sameNumber) {
-      setTenzies(true)
-    }else{setTenzies(false)}
-  }, [Value]);
-
-  const Refresh = () => {
-    setValue(RandomNumber())
+function getRandomNumber() {
+    return Math.floor(Math.random() * 6) + 1;
 }
 
-  return (
-    <div>
-       <main className="wrapper">
-        <Header tenzies={tenzies} />
-        <RenderDie
-          Value={Value}
-          handleToggle={toggleIsHeld}
-          tenzies={tenzies}
-        />
-        <RollBtn handleClick={regenerateNewArray} tenzies={tenzies} handleRefresh={Refresh} />
-      </main>
-    </div>
-  );
+function RandomNumber() {
+    return Array.from(Array(10).keys()).map((_, index) => ({
+        value: getRandomNumber(),
+        isHeld: false,
+        id: index,
+    }));
+}
+
+function MainContent() {
+    const [Value, setValue] = useState(RandomNumber());
+
+    const regenerateNewArray = () => {
+        setValue((value) => {
+            return value.map((e, index) => {
+                if (e.isHeld) {
+                    return e;
+                } else {
+                    e.value = getRandomNumber();
+                    e.id = index * 10; // i dont know why this is like this though
+                    return e;
+                }
+            });
+        });
+    };
+
+    const toggleIsHeld = (id) => {
+        setValue((prevState) => {
+            return prevState.map((e) => (e.id === id ? { ...e, isHeld: !e.isHeld } : e));
+        });
+    };
+
+    const [tenzies, setTenzies] = useState(false);
+
+    console.log(Value);
+
+    useEffect(() => {
+        console.log("dice state changed");
+        const firstNumber = Value[0].value;
+        const sameNumber = Value.every((e) => e.value === firstNumber && e.isHeld === true);
+
+        if (sameNumber) {
+            setTenzies(true);
+        } else {
+            setTenzies(false);
+        }
+    }, [Value]);
+
+    const Refresh = () => {
+        setValue(RandomNumber());
+    };
+
+    return (
+        <div>
+            <main className="wrapper">
+                <Header tenzies={tenzies} />
+                <RenderDie Value={Value} handleToggle={toggleIsHeld} tenzies={tenzies} />
+                <RollBtn
+                    handleClick={regenerateNewArray}
+                    tenzies={tenzies}
+                    handleRefresh={Refresh}
+                />
+            </main>
+        </div>
+    );
 }
 
 export default MainContent;
